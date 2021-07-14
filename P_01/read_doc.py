375a0ec0e1ce845cb6ea1929e49a9312c3dd2546
boa0107 <2446912060@qq.com>
2021-07-14 10:28:04 +0800

    aaa

--- P_01/read_doc.py
+++ P_01/read_doc.py
@@ -12,8 +12,23 @@ def read_doc(filename, sheetname, MaxLine=0):
     y = sheet.max_row
 
     content = []
-    for x in range(1, x + 1):
+    if MaxLine == 0:
+        for i in range(1, x + 1):
+            c = []
+            for j in range(1, y + 1):
+                c.append(sheet.cell(i, j).value)
+            content.append(c)
+    else:
         c = []
+        for j in range(1, y + 1):
+            c.append(sheet.cell(1, j).value)
+        content.append(c)
+        for i in range(x + 1 - MaxLine, x + 1):
+            c = []
+            for j in range(1, y + 1):
+                c.append(sheet.cell(i, j).value)
+            content.append(c)
+    return content
 
 
 def read_dict(filename, sheetname):
@@ -25,8 +40,8 @@ def read_dict(filename, sheetname):
         content = sheet.cell(i, 1).value
         if content != None:
             key = content
-            dct[key]=[]
-            val = sheet.cell(i,2).value
+            dct[key] = []
+            val = sheet.cell(i, 2).value
             dct[key].append(val)
         else:
             val = sheet.cell(i, 2).value
